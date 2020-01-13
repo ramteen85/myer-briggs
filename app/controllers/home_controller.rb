e79dec323fef0ad14a8ae592f15309aa38f62af4
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-01-13 17:04:59 +1100

    fixing up the registration interface and form validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,40 @@ class HomeController < ApplicationController
     end
 
     def register
+
+        hold = false
+
         #not complete
-        if params[:username]
-            flash[:flashuser] = params[:username]
-            redirect_to "/login" 
+        if !params[:username]
+            flash[:user_error] = "Please enter a username"
         end
+
+        if !params[:fullname]
+            flash[:name_error] = "Full name cannot be blank"   
+        end
+
+        if !params[:country]
+            flash[:country_error] = "Country cannot be blank"   
+        end
+
+        if !params[:state]
+            flash[:state_error] = "State name cannot be blank"   
+        end
+
+        if !params[:city]
+            flash[:city_error] = "City cannot be blank"   
+        end
+
+        if !params[:password]
+            flash[:password_error] = "Password cannot be blank."   
+        end
+
+        if !params[:confirm_password]
+            flash[:confirm_password_error] = "Please confirm your password."   
+        end
+
+
+        # redirect_to "/login"
 
         
     end
